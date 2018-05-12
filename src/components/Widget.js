42470b70a87fe9eb7a2bf13f93eadc1a5e083f0c
Shane <shanedavidnielsen@gmail.com>
2018-05-11 19:15:09 -0700

    Change widgets based on class and json

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,18 +1,47 @@
 import React, {Component} from 'react';
 
-var json = {
+var CSE110 = {
                "name":"GradeScope",
                "class":"CSE 140",
-               "grades":[ "HW1: 92.0 / 100.0", "HW2: 88.0 / 100.0" ]
+               "grades":[ "HW1: 2.0 / 8.0", "Midterm: 88.0 / 400.0" ]
+               }
+var CSE100 = {
+               "name":"GradeScope",
+               "class":"CSE 140",
+               "grades":[ "Midterm 1: 92.0 / 100.0", "Midterm 2: 88.0 / 100.0" ]
+               }
+var CSE20 = {
+               "name":"GradeScope",
+               "class":"CSE 140",
+               "grades":[ "course grade: 92.0 / 100.0" ]
+               }
+var CSE30 = {
+               "name":"GradeScope",
+               "class":"CSE 140",
+               "grades":[ "Widgets are cool" ]
                }
 
-var nam = "test";
+var nam = "yest";
+var course;
+
 
 class Widget extends Component {
     constructor(name) {
       { /*  super(prop); */ }
         super();
         nam = name;
+
+        if(name == "CSE100") {
+          course = CSE100;
+        }
+        if(name == "CSE110") {
+          course = CSE110;
+        }
+        if(name == "CSE30") {
+          course = CSE30;
+        }
+
+
         this.state = {
             courses: ['CSE100', 'CSE110', 'CSE20', 'CSE30', 'CSE 140'],
             urls: ['http://www.gradesource.com/reports/3586/29656/index.html', 'http://www.gradesource.com/reports/7/29889/index.html', 'http://www.gradesource.com/reports/346/29648/index.html', 'http://www.gradesource.com/reports/288/29652/index.html'],
@@ -35,10 +64,10 @@ class Widget extends Component {
                     <div className="w-top">
                     </div>
                     <div key={arrayIndex}>{nam}</div>
-                    <div key={arrayIndex}>{json.name}</div>
-                    <div key={arrayIndex}>{json.grades[0]}</div>
-                    <div key={arrayIndex}>{json.grades[1]}</div>
-                    <div key={arrayIndex}>{json.grades[2]}</div>
+                    <div key={arrayIndex}>{course.name}</div>
+                    <div key={arrayIndex}>{course.grades[0]}</div>
+                    <div key={arrayIndex}>{course.grades[1]}</div>
+                    <div key={arrayIndex}>{course.grades[2]}</div>
                 </div>
             </div>
                 )
